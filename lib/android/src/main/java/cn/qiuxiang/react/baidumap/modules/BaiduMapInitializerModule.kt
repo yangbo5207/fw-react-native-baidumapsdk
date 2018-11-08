5f70fc0294f0eeba6aecffae367943c457a83835
阳波 <1832064870@qq.com>
2018-11-08 18:19:25 +0800

    update

--- lib/android/src/main/java/cn/qiuxiang/react/baidumap/modules/BaiduMapInitializerModule.kt
+++ lib/android/src/main/java/cn/qiuxiang/react/baidumap/modules/BaiduMapInitializerModule.kt
@@ -15,12 +15,16 @@ class BaiduMapInitializerModule(private val context: ReactApplicationContext) : 
         private var promise: Promise? = promise
 
         override fun onReceive(context: Context, intent: Intent) {
+          try {
             if (intent.action == SDK_BROADTCAST_ACTION_STRING_PERMISSION_CHECK_OK) {
                 promise?.resolve(null)
             } else {
                 val code = intent.getIntExtra(SDK_BROADTCAST_INTENT_EXTRA_INFO_KEY_ERROR_CODE, 0)
                 promise?.reject(code.toString(), intent.action)
             }
+          } catch (Exception e) {
+            Log.info('百度地图初始化异常.')
+          }
         }
     }
 
